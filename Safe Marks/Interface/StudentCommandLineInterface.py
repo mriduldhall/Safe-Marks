f2f4dd1db895aa97d80a0c571e0ff3bdfa156dec
mriduldhall <mriduldhall1@gmail.com>
2021-02-16 13:50:14 +0000

    Cleaned up and improved student CLI logic

--- Safe Marks/Interface/StudentCommandLineInterface.py
+++ Safe Marks/Interface/StudentCommandLineInterface.py
@@ -3,6 +3,8 @@ class CLI:
         self.student = student
         self.admin = admin
         self.archive = archive
+        self.menu_choice = None
+        self.menu_dict = None
 
     def initiate(self):
         exit_condition = False
@@ -10,40 +12,46 @@ class CLI:
         valid_input = False
         while (exit_condition is False) or (valid_input is False):
             save_needed = True
-            if (self.admin is False) and (self.archive is False):
-                student_menu_choice = input(
-                    "Enter 1 to edit marks of the student, 2 to get student details, 3 to get mark sheet details, 4 to get marks, 5 to leave this menu:")
-                student_menu_dict = self.student.student_menu_dict
-            elif (self.admin is True) and (self.archive is False):
-                student_menu_choice = input(
-                    "Enter 1 to edit marks of the student, 2 to get student details, 3 to get mark sheet details, 4 to get marks, 5 to edit student details, 6 to remove student from school(archive), 7 to permanently delete student 8 to leave this menu:")
-                student_menu_dict = self.student.admin_student_menu_dict
-            elif (self.admin is False) and (self.archive is True):
-                student_menu_choice = input(
-                    "Enter 1 to get student details, 2 to get mark sheet details, 3 to get marks and 4 to exit this menu:")
-                student_menu_dict = self.student.archive_student_menu_dict
-            else:
-                student_menu_choice = input(
-                    "Enter 1 to get student details, 2 to get mark sheet details, 3 to get marks, 4 to delete student and 5 to exit this menu:")
-                student_menu_dict = self.student.admin_archive_student_menu_dict
-            if student_menu_choice == str(len(student_menu_dict) + 1):
+            self.find_student_menu()
+            if self.menu_choice == str(len(self.menu_dict) + 1):
                 exit_condition = True
                 valid_input = True
-            elif (student_menu_choice == str(len(student_menu_dict))) and (self.admin is True):
-                student_menu_dict[student_menu_choice]()
+            elif (self.menu_choice == str(len(self.menu_dict))) and (self.admin is True):
+                self.menu_dict[self.menu_choice]()
                 exit_condition = True
                 valid_input = True
                 save_needed = False
-            elif (student_menu_choice > str(len(student_menu_dict) + 1)) or student_menu_choice < "1":
+            elif (self.menu_choice > str(len(self.menu_dict) + 1)) or self.menu_choice < "1":
                 print("Please enter a valid choice!")
                 valid_input = False
                 exit_condition = False
             else:
-                student_menu_dict[student_menu_choice]()
+                self.menu_dict[self.menu_choice]()
                 exit_condition = False
                 valid_input = True
-            if student_menu_choice in student_menu_dict.keys():
-                if student_menu_dict[student_menu_choice] == self.student.student_controller.archive:
-                    self.archive = True
+            self.check_special_conditions()
         if (save_needed is True) and (self.archive is False):
             self.student.student_controller.save_student_data()
+
+    def find_student_menu(self):
+        if (self.admin is False) and (self.archive is False):
+            self.menu_choice = input(
+                "Enter 1 to edit marks of the student, 2 to get student details, 3 to get mark sheet details, 4 to get marks, 5 to leave this menu:")
+            self.menu_dict = self.student.student_menu_dict
+        elif (self.admin is True) and (self.archive is False):
+            self.menu_choice = input(
+                "Enter 1 to edit marks of the student, 2 to get student details, 3 to get mark sheet details, 4 to get marks, 5 to edit student details, 6 to remove student from school(archive), 7 to permanently delete student 8 to leave this menu:")
+            self.menu_dict = self.student.admin_student_menu_dict
+        elif (self.admin is False) and (self.archive is True):
+            self.menu_choice = input(
+                "Enter 1 to get student details, 2 to get mark sheet details, 3 to get marks and 4 to exit this menu:")
+            self.menu_dict = self.student.archive_student_menu_dict
+        else:
+            self.menu_choice = input(
+                "Enter 1 to get student details, 2 to get mark sheet details, 3 to get marks, 4 to delete student and 5 to exit this menu:")
+            self.menu_dict = self.student.admin_archive_student_menu_dict
+
+    def check_special_conditions(self):
+        if self.menu_choice in self.menu_dict.keys():
+            if self.menu_dict[self.menu_choice] == self.student.student_controller.archive:
+                self.archive = True
